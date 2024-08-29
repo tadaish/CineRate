ecdbaf0df9c3f8a3421c2f0e541180203ae1c769
tadaish <tadaishi159@gmail.com>
2024-08-30 06:07:25 +0700

    Update Login layout

--- app/src/main/java/com/example/cinerate/user/LoginActivity.java
+++ app/src/main/java/com/example/cinerate/user/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -75,7 +76,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-        Button registerButton = findViewById(R.id.registerButton);
+        TextView registerButton = findViewById(R.id.registerButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
