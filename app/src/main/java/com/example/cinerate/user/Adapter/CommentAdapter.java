072b998b71214d19e0e21b5e07750c79835fa3e8
NgoTruongAn <anngo091101@gmail.com>
2024-08-29 11:55:53 +0700

    addcmt

---
+++ app/src/main/java/com/example/cinerate/user/Adapter/CommentAdapter.java
@@ -0,0 +1,48 @@
+package com.example.cinerate.user.Adapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.cinerate.R;
+import com.example.cinerate.models.Comment;
+import java.util.List;
+
+public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentViewHolder> {
+    private List<Comment> commentList;
+
+    public CommentAdapter(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+
+    @NonNull
+    @Override
+    public CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.comment_item, parent, false);
+        return new CommentViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CommentViewHolder holder, int position) {
+        Comment comment = commentList.get(position);
+        holder.commentContent.setText(comment.getContent());
+        holder.commentAuthor.setText("User ID: " + comment.getUser_id());  // Bạn có thể thay thế bằng tên người dùng
+    }
+
+    @Override
+    public int getItemCount() {
+        return commentList.size();
+    }
+
+    public static class CommentViewHolder extends RecyclerView.ViewHolder {
+        TextView commentContent, commentAuthor;
+
+        public CommentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            commentContent = itemView.findViewById(R.id.comment_content);
+            commentAuthor = itemView.findViewById(R.id.comment_author);
+        }
+    }
+}
+
--- app/src/main/java/com/example/cinerate/user/MovieDetails.java
+++ app/src/main/java/com/example/cinerate/user/MovieDetails.java
@@ -1,6 +1,6 @@
 package com.example.cinerate.user;
 
-import android.media.Image;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebChromeClient;
@@ -8,23 +8,35 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
 import com.example.cinerate.R;
+import com.example.cinerate.daos.CommentDAO;
+import com.example.cinerate.models.Comment;
+import com.example.cinerate.user.Adapter.CommentAdapter;
 import com.example.cinerate.utils.YouTubeUtils;
+import android.text.TextUtils;
+import android.widget.Toast;
 
-public class MovieDetails extends AppCompatActivity  {
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+public class MovieDetails extends AppCompatActivity {
     WebView webView;
-    TextView movieName;
-    Button playButton;
+    Button watchlist, postCommentButton;
+    RecyclerView commentRecyclerView;
+    EditText commentInput;
+    CommentDAO commentDAO;
+    List<Comment> commentsList;
+    CommentAdapter commentAdapter;
 
-    String mName, mImage,mId, mTrailerUrl;
+    String mId, mName, mImage, mTrailerUrl;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,31 +44,65 @@ public class MovieDetails extends AppCompatActivity  {
         setContentView(R.layout.activity_movie_details);
 
         webView = findViewById(R.id.movie_trailer);
-        movieName = findViewById(R.id.movie_name);
-        playButton = findViewById(R.id.play_button);
+        watchlist = findViewById(R.id.watch_list);
+        postCommentButton = findViewById(R.id.post_comment_button);
+        commentRecyclerView = findViewById(R.id.comment_recycler_view);
+        commentInput = findViewById(R.id.comment_input);
 
+        commentDAO = new CommentDAO(this);
+
+        // Load movie details and trailer
         mId = getIntent().getStringExtra("movieId");
+        if (mId == null || mId.isEmpty()) {
+            Toast.makeText(this, "Movie ID is missing", Toast.LENGTH_SHORT).show();
+            finish(); // Hoặc thực hiện các hành động phù hợp khác
+            return;
+        }
         mName = getIntent().getStringExtra("movieName");
-        mImage= getIntent().getStringExtra("poster_url");
+        mImage = getIntent().getStringExtra("poster_url");
         mTrailerUrl = getIntent().getStringExtra("trailerUrl");
-        movieName.setText(mName);
-        String videoId = YouTubeUtils.extractVideoId(mTrailerUrl);
 
+        setupTrailerWebView(mTrailerUrl);
 
-        // Configure WebView
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.setWebViewClient(new WebViewClient());
-        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        // Load comments for this movie
+        loadComments();
 
-        // Load YouTube IFrame Player API
-        String iframeCode = "<html><body style=\"margin:0;padding:0;\">" +
-                "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/" + videoId +
-                "\" frameborder=\"0\" allowfullscreen></iframe>" +
-                "</body></html>";
+        // Post comment functionality
+        postCommentButton.setOnClickListener(v -> {
+            int userId = getLoggedInUserId(); // Lấy ID của người dùng đăng nhập
+            String commentContent = commentInput.getText().toString().trim();
 
+            if (userId != -1 && !TextUtils.isEmpty(commentContent)) {
+                Comment newComment = new Comment(0, Integer.parseInt(mId), userId, commentContent, ""); // Bỏ qua created_at
+                commentDAO.addComment(newComment);
+                commentInput.setText(""); // Clear input field
+                loadComments(); // Reload comments
+            } else {
+                Toast.makeText(this, "Please log in and enter a comment.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadComments() {
+        commentsList = commentDAO.getAllCommentsByMovie(Integer.parseInt(mId));
+        commentAdapter = new CommentAdapter(commentsList);
+        commentRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        commentRecyclerView.setAdapter(commentAdapter);
+    }
+
+    private int getLoggedInUserId() {
+        SharedPreferences sharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        return sharedPreferences.getInt("LoggedInUserId", -1);
+    }
+
+    private void setupTrailerWebView(String trailerUrl) {
+        String videoId = YouTubeUtils.extractVideoId(trailerUrl);
+        String iframeCode = "<html><body style=\"margin:0;padding:0;\"><iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/"
+                + videoId + "\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
+
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.loadData(iframeCode, "text/html", "UTF-8");
     }
 }
+
+
